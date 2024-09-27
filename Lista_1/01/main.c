8edb69c574e4c097d5b0169c838e97affe8d9a08
BMSales <bruno.m.sales03@gmail.com>
2024-09-26 22:03:06 -0300

    Added more sorting algorithms and added algorithm analysis tools

--- Lista_1/01/main.c
+++ Lista_1/01/main.c
@@ -5,6 +5,64 @@
 
 int main(){
 	srand(clock());
-	int size = 100000;
-	int* array = Array_Init(size);
+
+	int size = 100000000;
+	int* array_raw = Array_Init_Random(size);
+	int* array_sort = Array_Init_Empty(size);
+
+	_operations* operations = Operations_Init();
+	clock_t start, end;
+	float seconds;
+
+	Array_Copy(array_raw, array_sort, size);
+
+	printf("Merge Sort\n");
+	start = clock();
+	Merge_Sort(array_sort, size, operations);
+	end = clock();
+	seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("time in milliseconds: %f\n", 1000 * seconds);
+	printf("number of comparisons: %lld\n", operations->comp);
+	printf("number of attributions: %lld\n\n", operations->attrib);
+
+	Array_Copy(array_raw, array_sort, size);
+	Operations_Reset(operations);
+
+	printf("Quick Sort\n");
+	start = clock();
+	Quick_Sort(array_sort, size, operations);
+	end = clock();
+	seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("time in milliseconds: %f\n", 1000 * seconds);
+	printf("number of comparisons: %lld\n", operations->comp);
+	printf("number of attributions: %lld\n\n", operations->attrib);
+
+	Array_Copy(array_raw, array_sort, size);
+	Operations_Reset(operations);
+
+	printf("Heap Sort\n");
+	start = clock();
+	Heap_Sort(array_sort, size, operations);
+	end = clock();
+	seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("time in milliseconds: %f\n", 1000 * seconds);
+	printf("number of comparisons: %lld\n", operations->comp);
+	printf("number of attributions: %lld\n\n", operations->attrib);
+
+	Array_Copy(array_raw, array_sort, size);
+	Operations_Reset(operations);
+
+	printf("Shell Sort\n");
+	start = clock();
+	Shell_Sort(array_sort, size, operations);
+	end = clock();
+	seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("time in milliseconds: %f\n", 1000 * seconds);
+	printf("number of comparisons: %lld\n", operations->comp);
+	printf("number of attributions: %lld\n\n", operations->attrib);
+
+	free(array_raw);
+	free(array_sort);
+	free(operations);
+	return 0;
 }
--- Lista_1/01/sort_algs.h
+++ Lista_1/01/sort_algs.h
@@ -1,7 +1,19 @@
 #pragma once
 
-int* Array_Init(int size);
+typedef struct _operations{
+	long long comp;
+	long long attrib;
+}_operations;
+
+_operations* Operations_Init();
+void Operations_Reset(_operations* operations);
+
+int* Array_Init_Empty(int size);
+int* Array_Init_Random(int size);
+void Array_Copy(int* array_1, int* array_2, int size);
 void Array_Print(int* array, int size);
-void Merge_Sort(int* array, int size);
-void Shell_Sort(int* array, int size);
-void Quick_Sort(int* array, int size);
+
+void Merge_Sort(int* array, int size, _operations* operations);
+void Shell_Sort(int* array, int size, _operations* operations);
+void Quick_Sort(int* array, int size, _operations* operations);
+void Heap_Sort(int* array, int size, _operations* operations);
